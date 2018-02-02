c93d048c5ef2c4686ad48c99c74419bb75e90f42
Gihong Kim <gyong1211@gmail.com>
2018-02-02 17:20:09 +0900

    Modify variable name

--- keyboard/hhkb/keymap_gyong.c
+++ keyboard/hhkb/keymap_gyong.c
@@ -36,7 +36,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      *       |   |     |                       |     |   |
      *       `-------------------------------------------'
-     */ 
+     */
     KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, TRNS,  \
            CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, TRNS,      \
            TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,PENT,            \
@@ -59,8 +59,8 @@ enum function_id {
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-#   define MODS_LCTRL_MASK   MOD_BIT(KC_LCTL)
-    static uint8_t isLCtrlPressed;
+#   define MAGIC_MOD        MOD_BIT(KC_LCTL)
+    static uint8_t isMagicModPressed;
     bool isOnlyLShift;
     bool isOnlyRShift;
     bool isOnlyLAlt;
@@ -110,13 +110,14 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
             break;
 
         case ARROW_H:
-            isLCtrlPressed = get_mods() & MODS_LCTRL_MASK;
+            isMagicModPressed = get_mods() & MAGIC_MOD;
+
             if (record->event.pressed) {
-                if (isLCtrlPressed) {
-                    del_mods(MOD_BIT(KC_LCTL));
+                if (isMagicModPressed) {
+                    del_mods(MAGIC_MOD);
                     add_key(KC_LEFT);
                     send_keyboard_report();
-                    add_mods(MOD_BIT(KC_LCTL));
+                    add_mods(MAGIC_MOD);
                 }
                 else {
                     add_key(KC_H);
@@ -124,12 +125,12 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
                 }
             }
             else {
-                if (isLCtrlPressed) {
-                    del_mods(MOD_BIT(KC_LCTL));
+                if (isMagicModPressed) {
+                    del_mods(MAGIC_MOD);
                     del_key(KC_LEFT);
                     del_key(KC_H);
                     send_keyboard_report();
-                    add_mods(MOD_BIT(KC_LCTL));
+                    add_mods(MAGIC_MOD);
                 }
                 else {
                     del_key(KC_LEFT);
@@ -140,14 +141,14 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
             break;
 
         case ARROW_J:
-            isLCtrlPressed  = get_mods() & MODS_LCTRL_MASK;
+            isMagicModPressed  = get_mods() & MAGIC_MOD;
 
             if (record->event.pressed) {
-                if (isLCtrlPressed) {
-                    del_mods(MOD_BIT(KC_LCTL));
+                if (isMagicModPressed) {
+                    del_mods(MAGIC_MOD);
                     add_key(KC_DOWN);
                     send_keyboard_report();
-                    add_mods(MOD_BIT(KC_LCTL));
+                    add_mods(MAGIC_MOD);
                 }
                 else {
                     add_key(KC_J);
@@ -155,12 +156,12 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
                 }
             }
             else {
-                if (isLCtrlPressed) {
-                    del_mods(MOD_BIT(KC_LCTL));
+                if (isMagicModPressed) {
+                    del_mods(MAGIC_MOD);
                     del_key(KC_DOWN);
                     del_key(KC_J);
                     send_keyboard_report();
-                    add_mods(MOD_BIT(KC_LCTL));
+                    add_mods(MAGIC_MOD);
                 }
                 else {
                     del_key(KC_DOWN);
@@ -171,14 +172,14 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
             break;
 
         case ARROW_K:
-            isLCtrlPressed = get_mods() & MODS_LCTRL_MASK;
+            isMagicModPressed = get_mods() & MAGIC_MOD;
 
             if (record->event.pressed) {
-                if (isLCtrlPressed) {
-                    del_mods(MOD_BIT(KC_LCTL));
+                if (isMagicModPressed) {
+                    del_mods(MAGIC_MOD);
                     add_key(KC_UP);
                     send_keyboard_report();
-                    add_mods(MOD_BIT(KC_LCTL));
+                    add_mods(MAGIC_MOD);
                 }
                 else {
                     add_key(KC_K);
@@ -186,12 +187,12 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
                 }
             }
             else {
-                if (isLCtrlPressed) {
-                    del_mods(MOD_BIT(KC_LCTL));
+                if (isMagicModPressed) {
+                    del_mods(MAGIC_MOD);
                     del_key(KC_UP);
                     del_key(KC_K);
                     send_keyboard_report();
-                    add_mods(MOD_BIT(KC_LCTL));
+                    add_mods(MAGIC_MOD);
                 }
                 else {
                     del_key(KC_UP);
@@ -202,14 +203,14 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
             break;
 
         case ARROW_L:
-            isLCtrlPressed = get_mods() & MODS_LCTRL_MASK;
+            isMagicModPressed = get_mods() & MAGIC_MOD;
 
             if (record->event.pressed) {
-                if (isLCtrlPressed) {
-                    del_mods(MOD_BIT(KC_LCTL));
+                if (isMagicModPressed) {
+                    del_mods(MAGIC_MOD);
                     add_key(KC_RIGHT);
                     send_keyboard_report();
-                    add_mods(MOD_BIT(KC_LCTL));
+                    add_mods(MAGIC_MOD);
                 }
                 else {
                     add_key(KC_L);
@@ -217,12 +218,12 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
                 }
             }
             else {
-                if (isLCtrlPressed) {
-                    del_mods(MOD_BIT(KC_LCTL));
+                if (isMagicModPressed) {
+                    del_mods(MAGIC_MOD);
                     del_key(KC_RIGHT);
                     del_key(KC_L);
                     send_keyboard_report();
-                    add_mods(MOD_BIT(KC_LCTL));
+                    add_mods(MAGIC_MOD);
                 }
                 else {
                     del_key(KC_RIGHT);
